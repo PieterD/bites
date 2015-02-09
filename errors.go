c5517d057d7d78298b1e6762f120783358862928
Pieter Droogendijk <pieter@binky.org.uk>
2015-02-09 22:27:24 +0100

    Added expect integers

--- errors.go
+++ errors.go
@@ -44,3 +44,115 @@ type ErrorExpectSlice struct {
 func (err ErrorExpectSlice) Error() string {
 	return fmt.Sprintf("Expected %s (%#v), got (%#v)", "[]byte", err.Exp, err.Got)
 }
+
+type ErrorExpectInt8 struct {
+	Exp, Got int8
+}
+
+func (err ErrorExpectInt8) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "int8", err.Exp, err.Got)
+}
+
+type ErrorExpectUint8 struct {
+	Exp, Got uint8
+}
+
+func (err ErrorExpectUint8) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "uint8", err.Exp, err.Got)
+}
+
+type ErrorExpectInt16 struct {
+	Exp, Got int16
+}
+
+func (err ErrorExpectInt16) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "int16", err.Exp, err.Got)
+}
+
+type ErrorExpectUint16 struct {
+	Exp, Got uint16
+}
+
+func (err ErrorExpectUint16) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "uint16", err.Exp, err.Got)
+}
+
+type ErrorExpectInt32 struct {
+	Exp, Got int32
+}
+
+func (err ErrorExpectInt32) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "int32", err.Exp, err.Got)
+}
+
+type ErrorExpectUint32 struct {
+	Exp, Got uint32
+}
+
+func (err ErrorExpectUint32) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "uint32", err.Exp, err.Got)
+}
+
+type ErrorExpectInt64 struct {
+	Exp, Got int64
+}
+
+func (err ErrorExpectInt64) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "int64", err.Exp, err.Got)
+}
+
+type ErrorExpectUint64 struct {
+	Exp, Got uint64
+}
+
+func (err ErrorExpectUint64) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "uint64", err.Exp, err.Got)
+}
+
+type ErrorExpectFloat32 struct {
+	Exp, Got float32
+}
+
+func (err ErrorExpectFloat32) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "float32", err.Exp, err.Got)
+}
+
+type ErrorExpectFloat64 struct {
+	Exp, Got float64
+}
+
+func (err ErrorExpectFloat64) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "float64", err.Exp, err.Got)
+}
+
+type ErrorExpectComplex64 struct {
+	Exp, Got complex64
+}
+
+func (err ErrorExpectComplex64) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "complex64", err.Exp, err.Got)
+}
+
+type ErrorExpectComplex128 struct {
+	Exp, Got complex128
+}
+
+func (err ErrorExpectComplex128) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "complex128", err.Exp, err.Got)
+}
+
+type ErrorExpectVarInt struct {
+	Exp, Got int64
+}
+
+func (err ErrorExpectVarInt) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "varint", err.Exp, err.Got)
+}
+
+type ErrorExpectVarUint struct {
+	Exp, Got uint64
+}
+
+func (err ErrorExpectVarUint) Error() string {
+	return fmt.Sprintf("Expected %s (%s), got (%s)", "varuint", err.Exp, err.Got)
+}
---
+++ expectints.go
@@ -0,0 +1,181 @@
+package bites
+
+func (b Bites) ExpectInt8(expect int8) Bites {
+	var i int8
+	b = b.GetInt8(&i)
+	if i != expect {
+		panic(ErrorExpectInt8{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectUint8(expect uint8) Bites {
+	var i uint8
+	b = b.GetUint8(&i)
+	if i != expect {
+		panic(ErrorExpectUint8{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectInt16(expect int16) Bites {
+	var i int16
+	b = b.GetInt16(&i)
+	if i != expect {
+		panic(ErrorExpectInt16{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectInt16LE(expect int16) Bites {
+	var i int16
+	b = b.GetInt16LE(&i)
+	if i != expect {
+		panic(ErrorExpectInt16{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectUint16(expect uint16) Bites {
+	var i uint16
+	b = b.GetUint16(&i)
+	if i != expect {
+		panic(ErrorExpectUint16{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectUint16LE(expect uint16) Bites {
+	var i uint16
+	b = b.GetUint16LE(&i)
+	if i != expect {
+		panic(ErrorExpectUint16{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectInt32(expect int32) Bites {
+	var i int32
+	b = b.GetInt32(&i)
+	if i != expect {
+		panic(ErrorExpectInt32{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectInt32LE(expect int32) Bites {
+	var i int32
+	b = b.GetInt32LE(&i)
+	if i != expect {
+		panic(ErrorExpectInt32{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectUint32(expect uint32) Bites {
+	var i uint32
+	b = b.GetUint32(&i)
+	if i != expect {
+		panic(ErrorExpectUint32{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectUint32LE(expect uint32) Bites {
+	var i uint32
+	b = b.GetUint32LE(&i)
+	if i != expect {
+		panic(ErrorExpectUint32{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectInt64(expect int64) Bites {
+	var i int64
+	b = b.GetInt64(&i)
+	if i != expect {
+		panic(ErrorExpectInt64{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectInt64LE(expect int64) Bites {
+	var i int64
+	b = b.GetInt64LE(&i)
+	if i != expect {
+		panic(ErrorExpectInt64{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectUint64(expect uint64) Bites {
+	var i uint64
+	b = b.GetUint64(&i)
+	if i != expect {
+		panic(ErrorExpectUint64{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectUint64LE(expect uint64) Bites {
+	var i uint64
+	b = b.GetUint64LE(&i)
+	if i != expect {
+		panic(ErrorExpectUint64{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectFloat32(expect float32) Bites {
+	var i float32
+	b = b.GetFloat32(&i)
+	if i != expect {
+		panic(ErrorExpectFloat32{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectFloat64(expect float64) Bites {
+	var i float64
+	b = b.GetFloat64(&i)
+	if i != expect {
+		panic(ErrorExpectFloat64{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectComplex64(expect complex64) Bites {
+	var i complex64
+	b = b.GetComplex64(&i)
+	if i != expect {
+		panic(ErrorExpectComplex64{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectComplex128(expect complex128) Bites {
+	var i complex128
+	b = b.GetComplex128(&i)
+	if i != expect {
+		panic(ErrorExpectComplex128{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectVarInt(expect int64) Bites {
+	var i int64
+	b = b.GetVarInt(&i, nil)
+	if i != expect {
+		panic(ErrorExpectVarInt{Exp: expect, Got: i})
+	}
+	return b
+}
+
+func (b Bites) ExpectVarUint(expect uint64) Bites {
+	var i uint64
+	b = b.GetVarUint(&i, nil)
+	if i != expect {
+		panic(ErrorExpectVarUint{Exp: expect, Got: i})
+	}
+	return b
+}
