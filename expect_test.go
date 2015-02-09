9d5d3207c5b0f6f895d9d0317d0dbd7f2eda78a5
Pieter Droogendijk <pieter@binky.org.uk>
2015-02-09 23:08:14 +0100

    Add integers to expect test

--- expect_test.go
+++ expect_test.go
@@ -41,3 +41,26 @@ func TestExpectFail(t *testing.T) {
 		Empty().PutString("hello world").ExpectSlice(slice)
 	}()
 }
+
+func TestExpectInts(t *testing.T) {
+	b := Empty()
+	b = b.PutInt8(-100).PutUint8(200)
+	b = b.PutInt16(30000).PutInt16LE(-30000).PutUint16(60000).PutUint16LE(50000)
+	b = b.PutInt32(2000000000).PutUint32(4000000000)
+	b = b.PutInt32LE(1000000000).PutUint32LE(1234567890)
+	b = b.PutInt64(2000000000).PutUint64(4000000000)
+	b = b.PutInt64LE(1000000000).PutUint64LE(1234567890)
+	b = b.PutFloat32(0.12345).PutFloat64(1.23456)
+	b = b.PutComplex64(0.12345 + 1234565i).PutComplex128(1.23456 + 6.2341i)
+	b = b.PutVarInt(-91234567789851, nil).PutVarUint(4861749834769813798, nil)
+
+	b = b.ExpectInt8(-100).ExpectUint8(200)
+	b = b.ExpectInt16(30000).ExpectInt16LE(-30000).ExpectUint16(60000).ExpectUint16LE(50000)
+	b = b.ExpectInt32(2000000000).ExpectUint32(4000000000)
+	b = b.ExpectInt32LE(1000000000).ExpectUint32LE(1234567890)
+	b = b.ExpectInt64(2000000000).ExpectUint64(4000000000)
+	b = b.ExpectInt64LE(1000000000).ExpectUint64LE(1234567890)
+	b = b.ExpectFloat32(0.12345).ExpectFloat64(1.23456)
+	b = b.ExpectComplex64(0.12345 + 1234565i).ExpectComplex128(1.23456 + 6.2341i)
+	b = b.ExpectVarInt(-91234567789851, nil).ExpectVarUint(4861749834769813798, nil)
+}
