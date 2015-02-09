a28e79dd130145fb97b5a7b756e7f21f77133f87
Pieter Droogendijk <pieter@binky.org.uk>
2015-02-10 00:02:09 +0100

    Fixed examples

--- example_test.go
+++ example_test.go
@@ -6,14 +6,7 @@ import (
 	"github.com/PieterD/bites"
 )
 
-func ExampleStringOps() {
-	// Chain operations to construct your buffer.
-	b := bites.New().PutByte('H').PutRune('e', nil).PutString("llo ").PutSlice([]byte("world!"))
-	fmt.Println(b.String())
-	// Output: Hello world!
-}
-
-func ExampleExpect() {
+func ExampleBites() {
 	// Chain operations to construct your buffer.
 	var hash [32]byte
 	b := bites.New().PutString("some string").PutUint32(513).PutVar(912345)
@@ -25,6 +18,15 @@ func ExampleExpect() {
 	var str string
 	r := b.Skip(11).ExpectUint32(513).ExpectVarInt(912345, nil).GetByte(&qbyte)
 	r = r.GetSliceCopy(hashcopy[:]).GetString(&str, 7).ExpectString(" one")
+
 	fmt.Printf("%c %s\n", qbyte, str)
 	// Output: Q another
+}
+
+func ExampleWriter() {
+	b := bites.New().PutString("Hello")
+	w := bites.NewWriter(b)
+	fmt.Fprintf(w, ", world!")
+	fmt.Println(w.Bites().String())
+	// Output: Hello, world!
 }
