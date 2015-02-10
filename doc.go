b71d6dd7d31d4fbf04911ecdfac91ca322b17ea8
Pieter Droogendijk <pieter@binky.org.uk>
2015-02-11 00:50:53 +0100

    Added doc.go

--- doc.go
+++ doc.go
@@ -1,6 +1,5 @@
-// Package bites deals with byte slices.
-// Its purpose is to make it easier to marshal and unmarshal the various basic types to and from byte slices.
-//
-// Most of these methods do not allocate, and many of them are inlined.
+// Most of its methods do not allocate unnecessarily, and many of them are inlined.
 // When a method does allocate, it is noted in the docs.
+
+// Package bites implements a way to easily pack and unpack any primitive type into a byte slice.
 package bites
